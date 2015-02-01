070cee13d35a793e3c1e8f17aee5fdd91ba830a8
eabryant <bryant.evan.a@gmail.com>
2015-01-31 19:30:39 -0600

    added slide animations and board re-rendering

--- board.py
+++ board.py
@@ -11,12 +11,13 @@ from block import Block
 class Slide(Animation):
     _unlock_callback = None
 
-    def __init__(self, **kwargs):
+    def __init__(self, block, **kwargs):
         super(Slide, self).__init__(duration=.25, **kwargs)
+        self.block = block
         self.bind(on_complete=_unlock_callback)
 
     @staticmethod
-    def set_unlock(callback):
+    def set_unlock_callback(callback):
         global _unlock_callback
         _unlock_callback = callback
 
@@ -29,13 +30,9 @@ class Board(FloatLayout):
         self.h = Window.size[1] - self.w - (Window.size[1] - self.w) / 2
         self.d = int(self.w * .25)
         self.grid = self._empty_grid()
+        self.slide_queue = []
 
-        Slide.set_unlock(self._unlock)
-
-        self._slide_r = Slide(x=self._to_gx(3))
-        self._slide_l = Slide(x=self._to_gx(0))
-        self._slide_t = Slide(y=self._to_gy(3))
-        self._slide_b = Slide(y=self._to_gy(0))
+        Slide.set_unlock_callback(self._unlock)
 
         super(Board, self).__init__(**kwargs)
         self.reset()
@@ -48,49 +45,78 @@ class Board(FloatLayout):
 
         if fabs(dx) > fabs(dy):
             if dx > 0:
-                slide = self._slide_r
+                direction = 0
+                rotate = lambda: self.grid
+                unrotate = lambda: self.grid
             else:
-                slide = self._slide_l
+                direction = 1
+                rotate = self._transpose_flip
+                unrotate = self._transpose_flip
         else:
             if dy > 0:
-                slide = self._slide_t
+                direction = 2
+                rotate = self._transpose_left
+                unrotate = self._transpose_right
             else:
-                slide = self._slide_b
+                direction = 3
+                rotate = self._transpose_right
+                unrotate = self._transpose_left
 
-        for row in self.grid:
-            for block in row:
-                if block is None:
-                    continue
-                block.move(slide)
+        grid = rotate()
+        self.grid = self._move(grid, direction)
+        self.grid = unrotate()
 
-    def _move(self):
+        for slide in self.slide_queue:
+            slide.start(slide.block)
+        self.slide_queue = []
+
+    def _move(self, grid, dir):
         g = self._empty_grid()
 
         for yi in range(4):
             row = []
             for xi in range(4):
-                if not self.grid[yi][xi] is None:
-                    row.append(self.grid[yi][xi])
-            g[yi] = self._collapse(row)
+                if not grid[yi][xi] is None:
+                    row.append(grid[yi][xi])
+            g[yi] = self._collapse(row, dir)
 
-        self.grid = g
+        return g
 
-    def _collapse(self, irow):
+    def _collapse(self, irow, dir):
         row = []
+
+        horizontal = dir < 2
+        pos = dir % 2 == 0
 
         if len(irow) > 1:
             merge = 4
             i = len(irow) - 1
+            j = 3
             while i >= 0:
                 if irow[i].value == irow[i-1].value and merge != i:
                     row.append(Block(value=irow[i].value * 2))
+                    if horizontal:
+                        self.slide_queue.append(Slide(irow[i], x=self._to_gx(j if pos else 3-j)))
+                        self.slide_queue.append(Slide(irow[i-1], x=self._to_gx(j if pos else 3-j)))
+                    else:
+                        self.slide_queue.append(Slide(irow[i], y=self._to_gy(j if pos else 3-j)))
+                        self.slide_queue.append(Slide(irow[i-1], y=self._to_gy(j if pos else 3-j)))
                     merge = i
                     i -= 1
                 else:
                     row.append(Block(value=irow[i].value))
+                    if horizontal:
+                        self.slide_queue.append(Slide(irow[i], x=self._to_gx(j if pos else 3-j)))
+                    else:
+                        self.slide_queue.append(Slide(irow[i], y=self._to_gy(j if pos else 3-j)))
                 i -= 1
+                j -= 1
         elif len(irow) == 1:
             row.append(Block(value=irow[0].value))
+            if horizontal:
+                self.slide_queue.append(Slide(irow[0], x=self._to_gx(3 if pos else 0)))
+            else:
+                self.slide_queue.append(Slide(irow[0], y=self._to_gy(3 if pos else 0)))
 
         while len(row) < 4:
             row.append(None)
@@ -109,11 +135,18 @@ class Board(FloatLayout):
             x = randint(0, 3)
             y = randint(0, 3)
 
-        self.grid[y][x] = Block(pos=self._to_global(x, y))
+        self.grid[y][x] = Block(pos=self._to_global(x, y), value=2)
         self.add_widget(self.grid[y][x])
 
     def _unlock(self, *args):
         self.locked = False
+
+        self.clear_widgets()
+        for y in range(4):
+            for x in range(4):
+                if self.grid[y][x] is not None:
+                    self.grid[y][x].pos = (self._to_global(x, y))
+                    self.add_widget(self.grid[y][x])
 
     def _to_gx(self, x):
         return self.d * x
@@ -138,8 +171,6 @@ class Board(FloatLayout):
 
     def _transpose(self, i_offset, j_offset, flip):
         transpose = self._empty_grid()
-
-        print(self.grid)
 
         for i in range(4):
             for j in range(4):
